5ffce4abd467feb827e03867c96e12626a9571e4
Alex-wsl2 <abaekey@knights.ucf.edu>
2023-07-11 09:10:52 -0400

    Added basic pathway analysis to characterize results

--- static-analysis/generate_trees.py
+++ static-analysis/generate_trees.py
@@ -9,9 +9,8 @@ from src import influential_edge_ranking
 from src import generate_edge_types
 
 
-def generate_trees(g, edge_type, te_thresh):
+def generate_trees(g, edge_type, te_thresh, pathway_selection):
 
-    pathway_type="greedy" # options: summed, greedy, or None
     vis_lim = 2
     dep_lim = 7
     orig_nodes = g.nodes
@@ -35,8 +34,32 @@ def generate_trees(g, edge_type, te_thresh):
         ax.figure.savefig(f'{edge_type}_te_thresh{te_thresh}_root{root}.png')
 
 
+def generate_tree_data(g, edge_type, te_thresh, pathway_type):
+
+    vis_lim = 2
+    dep_lim = 7
+    orig_nodes = g.nodes
+
+    root_nodes = [x for x in g.nodes() if g.out_degree(x) > 0]
+    g_df = nx.to_pandas_edgelist(g, source='Source', target='Target')
+    
+    root_graphs = {}
+    for in_root in root_nodes:
+        root_df = te_rollout.te_rollout(in_root, g_df, vis_lim)
+        g = nx.from_pandas_edgelist(root_df, 'Source', 'Target', [edge_type], create_using=nx.DiGraph())
+        root_graphs.update({in_root:g})
+
+    # Generate tree information in for of lists (1 entry per root node)
+    rnodes, xtrees, xpathways, xcolormap_nodes, xcolormap_edges, xpos = htrees.htrees(root_graphs, edge_type, te_thresh, vis_lim, dep_lim, orig_nodes, path=pathway_type) 
+
+    return xtrees, xpathways
+
+
+
+'''
 # input variables
-edge_type = 'UM_TM' # options: ...
+edge_type = 'UF_UM' # options: ...
+pathway_type="greedy" # options: summed, greedy, or None
 
 # Dataframe of TE network
 cascade_df = pd.read_csv('data/actor_te_edges_2018_03_01_2018_05_01.csv')
@@ -55,5 +78,5 @@ g = nx.from_pandas_edgelist(graph_df, 'Source', 'Target', [edge_type], create_us
 nx.relabel_nodes(g, actors, copy=False)
 
 generate_trees(g, edge_type, te_thresh)
-
+'''
 
---
+++ static-analysis/pathway_analysis.py
@@ -0,0 +1,55 @@
+from generate_trees import generate_tree_data
+import networkx as nx
+import pandas as pd
+from src import generate_edge_types
+import matplotlib.pyplot as plt
+
+# input variables
+edge_type = 'UF_UM' # options: ...
+pathway_selection = "greedy"
+
+# Dataframe of TE network
+cascade_df = pd.read_csv('data/actor_te_edges_2018_03_01_2018_05_01.csv')
+cascade_df = cascade_df.loc[(cascade_df['Source'] > 1.) & (cascade_df['Target'] > 1.) & (cascade_df['Source']<101.) & (cascade_df['Target']<101.)]
+
+# Dict of actor names (v2/v4/dynamic)
+actor_df = pd.read_csv('data/actors_v4.csv')
+actors = dict(zip(actor_df.actor_id, actor_df.actor_label))
+
+#te_thresh, graph_df = auto_threshold.auto_threshold(cascade_df, edge_type, 80, return_df=True)
+#te_thresh = auto_threshold.auto_threshold(cascade_df, edge_type, 80, return_df=False)
+
+# Create df to store results
+infl_df = pd.DataFrame()
+
+edge_types = generate_edge_types.generate_edge_types() 
+pathway_lengths = {}
+for edge_type in edge_types:
+
+    te_thresh = 0.1
+    graph_df = cascade_df.loc[(cascade_df[edge_type] > te_thresh)]
+    g = nx.from_pandas_edgelist(graph_df, 'Source', 'Target', [edge_type], create_using=nx.DiGraph())
+    nx.relabel_nodes(g, actors, copy=False)
+    xtrees, xpathways = generate_tree_data(g, edge_type, te_thresh, pathway_selection)
+    pathway_lens = []
+    #for tree in xtrees:
+    #    print(tree)
+    for pathway in xpathways:
+        #print(len(pathway))
+        pathway_lens.append(len(pathway))
+    path_av = sum(pathway_lens)/len(pathway_lens)
+    pathway_lengths[edge_type] = path_av
+
+print(pathway_lengths)
+names = list(pathway_lengths.keys())
+values = list(pathway_lengths.values())
+
+plt.ion()
+plt.bar(range(len(pathway_lengths)), values)
+plt.xticks(range(len(pathway_lengths)), names, rotation='vertical')
+plt.ylabel('path length')
+plt.title('Average pathway length')
+plt.savefig('ave_path_lengths.png')
+
+
+
