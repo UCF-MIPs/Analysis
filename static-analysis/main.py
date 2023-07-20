f5c61ea65e34734c8dc742c819dd2c1e1b4a15a1
alexbaekey <abaekey@knights.ucf.edu>
2023-07-20 12:32:31 -0400

    Cleaned repo of plots, finished pathway analysis

--- static-analysis/main.py
+++ static-analysis/main.py
@@ -38,9 +38,8 @@ generate_trees.generate_tree_plots(g, edge_type, te_thresh)
 
 # MULTIRUN
 # input variables
-edge_types = generate_edge_types.generate_edge_types()
-
-pathway_selection="greedy" # options: summed, greedy, or None
+#edge_types = generate_edge_types.generate_edge_types()
+#pathway_selection="greedy" # options: summed, greedy, or None
 
 # Dataframe of TE network
 #Skripal
@@ -50,15 +49,15 @@ pathway_selection="greedy" # options: summed, greedy, or None
 #actors = dict(zip(actor_df.actor_id, actor_df.actor_label))
 
 #Ukraine
-cascade_df = pd.read_csv('data/Ukraine/Actor_TE_Edges_Ukraine_v1.csv')
-print('test1')
-actor_df = pd.read_csv('data/Ukraine/actors_Ukraine_v1.csv')
-print('test2')
-actors = dict(zip(actor_df.actor_id, actor_df.actor_label))
+#cascade_df = pd.read_csv('data/Ukraine/Actor_TE_Edges_Ukraine_v1.csv')
+#actor_df = pd.read_csv('data/Ukraine/actors_Ukraine_v1.csv')
+#actors = dict(zip(actor_df.actor_id, actor_df.actor_label))
 
 
+'''
+# Iterating through thresholds
 for edge_type in edge_types:
-    for te_thresh in [0.1, 0.2]:
+    for te_thresh in [0.2, 0.25, 0.3]:
         graph_df = cascade_df.loc[(cascade_df[edge_type] > te_thresh)]
         g = nx.from_pandas_edgelist(graph_df, 'Source', 'Target', [edge_type], create_using=nx.DiGraph())
         nx.relabel_nodes(g, actors, copy=False)
@@ -68,8 +67,52 @@ for edge_type in edge_types:
         plt.savefig(f'graphs/{edge_type}_{te_thresh}.png')
         plt.clf()
 
-        root_nodes = influential_node_ranking(g, pulltop=5, sorted_nodes=True)
+        root_nodes = influential_node_ranking.influential_node_ranking(g, pulltop=5, node_names=True)
         generate_trees.generate_tree_plots(g, edge_type, te_thresh, pathway_selection, root_nodes, dir_name='Ukraine_trees')
         plt.clf()
-    
+
+
+# Autothresholding 
+dataset = 'Ukr'
+
+for edge_type in edge_types:
+        
+    te_thresh, graph_df = auto_threshold.auto_threshold(cascade_df, edge_type, 120, return_df=True)
+    #graph_df = cascade_df.loc[(cascade_df[edge_type] > te_thresh)]
+    g = nx.from_pandas_edgelist(graph_df, 'Source', 'Target', [edge_type], create_using=nx.DiGraph())
+    nx.relabel_nodes(g, actors, copy=False)
+      
+    # graphs
+    nx.draw(g, pos=nx.circular_layout(g), with_labels=True)
+    plt.savefig(f'graphs/{dataset}_{edge_type}_{te_thresh}.png')
+    plt.clf()
+
+    root_nodes = influential_node_ranking.influential_node_ranking(g, pulltop=5, node_names=True)
+    generate_trees.generate_tree_plots(g, edge_type, te_thresh, pathway_selection, root_nodes, dir_name='Ukraine_trees')
+    plt.clf()
+''' 
        
+# Use in interface
+
+pathway_selection="greedy" # options: summed, greedy, or None
+edge_type = 'UM_TM'
+te_thresh = 0.1
+
+cascade_df = pd.read_csv('data/Ukraine/Actor_TE_Edges_Ukraine_v1.csv', usecols=edge_type)
+actor_df = pd.read_csv('data/Ukraine/actors_Ukraine_v1.csv')
+actors = dict(zip(actor_df.actor_id, actor_df.actor_label))
+
+#te_thresh, graph_df = auto_threshold.auto_threshold(cascade_df, edge_type, 120, return_df=True)
+graph_df = cascade_df.loc[(cascade_df[edge_type] > te_thresh)]
+
+g = nx.from_pandas_edgelist(graph_df, 'Source', 'Target', [edge_type], create_using=nx.DiGraph())
+nx.relabel_nodes(g, actors, copy=False)
+
+# Can replace 'influential_node_ranking with a single root node as a list
+# for example, root_nodes = ['Ian56789']
+root_nodes = influential_node_ranking.influential_node_ranking(g, pulltop=5, node_names=True)
+
+generate_trees.generate_tree_plots(g, edge_type, te_thresh, pathway_selection, root_nodes, dir_name='trees')
+ 
+
+
